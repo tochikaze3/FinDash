62391d9fbfb05035237b6b8329211b34f6955c14
T-Cee <81631627+tochikaze3@users.noreply.github.com>
2024-02-24 15:51:12 -0500

    completed dasboard card

---
+++ src/Components/Dashboard.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Container, Box, Paper, Typography, ThemeProvider, Styled } from "@mui/material";
+
+
+const Dashboard = () => {
+  return (
+    <Container maxWidth="sm" sx={{ bgcolor: "#000", height: "50vh", marginTop: "100px" }} >
+        
+        <nav>
+          
+      </nav>
+        
+        <Box component="section" sx={{ p:1, my:1 }}>
+            <Paper >
+      <Typography variant = "subtitle1"  sx={{ textAlign: "center", color: "primary.main"}}>
+        Welcome, Sophia
+      </Typography>
+      </Paper>
+      </Box>
+
+      <Box >
+      <Typography variant = "body1"  sx={{textAlign: "center", color: "primary.main"}}>
+       0000 SAVINGS xxxx3366-00
+      </Typography>
+      </Box>
+
+
+    <Box  sx={{ p:1}}>
+        <Paper background-color='' elevation = '2' variant='outlined' square={false} >
+            <Typography variant = "h1"  sx={{my:1, textAlign: "center", color: "primary.main"}}>
+       -$125,275.00
+      </Typography>
+        </Paper>
+        </Box>
+
+      <Box  sx={{
+        display: 'flex',
+        flexWrap: 'nowrap',
+        '& > :not(style)': {
+          width: 600,
+          height: 30,
+        },
+      }} >
+      <Typography variant = "subtitle2"  sx={{ textAlign: "center", color: "primary.main"}}>
+       AVAILABLE BALANCE
+      </Typography>
+      </Box>
+
+      <Box >
+      <Typography variant = "h6"  sx={{ textAlign: "center", color: "primary.main"}}>
+       Current Balance -$18,720
+      </Typography>
+      </Box>
+
+     
+
+    </Container>
+  )
+}
+
+export default Dashboard
