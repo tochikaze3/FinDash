1fea1d590a4c8ed2521626ad6ba11b598f5a87d5
T-Cee <81631627+tochikaze3@users.noreply.github.com>
2024-02-26 23:50:56 -0500

    done stuff added link to

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -72,7 +72,7 @@ export default function SignIn() {
               label="Remember me"
             />
             <Button
-           
+             href="/Dashboard" 
               type="submit"
               fullWidth
               variant="contained"
